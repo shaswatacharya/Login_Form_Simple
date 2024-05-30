61bce6d29328f47c7772748ba0970b986a09347a
Shaswat Acharya <shaswatacharya123@gmail.com>
2024-05-30 10:45:33 +0545

    Login Form Simple Design using Html,Css and JS

---
+++ main.js
@@ -0,0 +1,18 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    
+    if (username === "" || password === "") {
+        alert("Please enter both username and password.");
+        return;
+    }
+
+    
+    if (username === "user" && password === "pass") {
+        alert("Login successful!");
+       
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+}
